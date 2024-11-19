de9358c602cecf2906a4b376ecf7da2fee467eee
Muhammad Nazam Waleed <2483698@edu.vaniercollege.qc.ca>
2024-11-19 09:44:24 -0500

    Did everything asked but was not able to change the cannons so that they shoot

--- starter-final/Ladybug.java
+++ starter-final/Ladybug.java
@@ -14,5 +14,67 @@ public class Ladybug extends Actor
      */
     public void act()
     {
+        /* */
+        moveAndTurn();
+        Arrive();
+        if (isGameWon()) {
+            transitionToGameWonWorld();
+            Greenfoot.playSound("win.wav");
+        }
+    }
+
+    /**
+     * This method allows te player to move thel adybug, with the arrowkeys.
+     */
+    public void moveAndTurn()
+    {
+        move(0);
+        if (Greenfoot.isKeyDown("left")) {
+            move(3);
+        }
+        if (Greenfoot.isKeyDown("right")) {
+            move(-3);
+        }
+        if (Greenfoot.isKeyDown("up")) {
+            turn(3);
+        }
+        if (Greenfoot.isKeyDown("down")) {
+            turn(-3);
+        }
+    }
+
+    /**
+     * when the ladybug arrrives on the finishLine, this methode removes the finish line. 
+     */
+    public void Arrive()
+    {
+        Actor FinishLocation = getOneIntersectingObject(FinishLocation.class);
+        if (FinishLocation != null) {
+            World world = getWorld();
+            world.removeObject(FinishLocation);
+        }
+    }
+
+    /**
+     * When the finish line is removed this method ends the game and the game is won else the game continues.
+     */
+    public boolean isGameWon()
+    {
+        World world = getWorld();
+        if (world.getObjects(FinishLocation.class).isEmpty()) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    /**
+     * When the public boolean isGameWon returns true this methods allows the transition to the game won world.
+     */
+    public void transitionToGameWonWorld()
+    {
+        World gameWonWorld =  new  GameWonWorld();
+        Greenfoot.setWorld(gameWonWorld);
     }
 }
